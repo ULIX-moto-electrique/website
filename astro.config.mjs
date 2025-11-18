64d00cd7cc111aed2ae86d0b4292af86d03eabb7
Mathias Boudreau <mabo1123@ulaval.ca>
2025-11-18 18:25:53 -0500

    Update font

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,45 +12,23 @@ export default defineConfig({
   experimental: {
     fonts: [{
       provider: "local",
-      name: "InterLocal",
-      cssVariable: "--font-inter",
+      name: "syneLocal",
+      cssVariable: "--font-syne",
       variants: [
         {
           weight: 400,
           style: "normal",
-          src: ["./src/assets/fonts/Inter-Regular.woff2"]
+          src: ["./src/assets/fonts/Syne-Regular.woff2"]
         },
         {
           weight: 600,
           style: "normal",
-          src: ["./src/assets/fonts/Inter-SemiBold.woff2"]
+          src: ["./src/assets/fonts/Syne-SemiBold.woff2"]
         },
         {
           weight: 700,
           style: "normal",
-          src: ["./src/assets/fonts/Inter-Bold.woff2"]
-        }
-      ]
-    },
-    {
-      provider: "local",
-      name: "InterLocalDisplay",
-      cssVariable: "--font-inter-display",
-      variants: [
-        {
-          weight: 400,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-Regular.woff2"]
-        },
-        {
-          weight: 500,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-Medium.woff2"]
-        },
-        {
-          weight: 600,
-          style: "normal",
-          src: ["./src/assets/fonts/InterDisplay-SemiBold.woff2"]
+          src: ["./src/assets/fonts/Syne-Bold.woff2"]
         }
       ]
     }]
